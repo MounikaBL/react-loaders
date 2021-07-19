ab4231ab7cd677a67c37e0cc456aeb18123c0d5f
Mounika_Badadh <mounika.bl@in.abb.com>
2021-07-19 19:40:23 +0530

    Commiting 6 initial loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import Loader from './Loader';
 
-function App() {
+const loaders = [{
+  name: "Loader 1", id: 1
+}, {
+  name: "Loader 2", id: 2
+}, {
+  name: "Loader 3", id: 3
+}, {
+  name: "Loader 4", id: 4
+}, {
+  name: "Loader 5", id: 5
+}, {
+  name: "Loader 6", id: 6
+}]
+
+const App: React.FC<any> = (props: any) => {
+  const [index, setIndex] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper" >
+      <div className="heading">Loaders</div>
+      <div className="nav-bar">
+        <ul className="list">
+          {
+            loaders.map((loader: any, Iindex: number) => {
+              return (
+                <li key={loader.id} onClick={(e: any) => {
+                  setIndex(Iindex + 1);
+                }}
+                  className={`list-item ${Iindex + 1 === index ? "active" : ""}`}>{loader.name}</li>);
+            })
+          }
+        </ul>
+      </div>
+      <div className="loaders">
+        <Loader index={index} />
+      </div>
+    </div >
   );
 }
 
---
+++ src/Loader.tsx
@@ -0,0 +1,129 @@
+import React, { Component } from 'react'
+
+export const Loader1 = (): JSX.Element => {
+    return <div className="loader1">
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+        <div className="loader">
+            <div className="loading">
+
+            </div>
+        </div>
+    </div>
+}
+
+export const Loader2 = (): JSX.Element => {
+    return (<div className="loader2">
+        <div className="loader">
+            <span className="dot"></span>
+            <span className="dot"></span>
+            <span className="dot"></span>
+            <span className="dot"></span>
+        </div>
+    </div>);
+}
+
+export const Loader3 = (): JSX.Element => {
+    return (
+        <div className="loader3">
+            <div className="inner-circle">
+                ☆
+            </div>
+        </div>
+    );
+}
+
+export const Loader4 = (): JSX.Element => {
+    return (<div className="loader4">
+        <div className="wrapper">
+            <div className="smallpin">
+            </div>
+            <div className="point">
+            </div>
+            <div className="bigpin">
+            </div>
+        </div>
+    </div>);
+}
+
+export const Loader5 = (): JSX.Element => {
+    return (<div className="loader5">
+        <div className="loader">
+
+        </div>
+    </div>);
+}
+
+export const Loader6 = (): JSX.Element => {
+    return (
+        <div className="loader6">
+            <div className="circle1">
+
+            </div>
+            <div className="circle2">
+
+            </div>
+        </div>);
+}
+
+export default class Loader extends Component<any, any> {
+
+    callComponent = (index: number) => {
+        switch (index) {
+            case 1:
+                return <Loader1 />
+            case 2:
+                return <Loader2 />
+            case 3:
+                return <Loader3 />
+            case 4:
+                return <Loader4 />
+            case 5:
+                return <Loader5 />
+            case 6:
+                return <Loader6 />
+            default:
+                return <Loader1 />
+        }
+    }
+    render() {
+        console.log("mmmmmmm", this.props.index);
+        return (
+            this.callComponent(this.props.index)
+        );
+    }
+}
